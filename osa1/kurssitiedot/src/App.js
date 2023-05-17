f184e099c1d0de07c9c506b735cd703ad762c47a
Santeri <santeri.oikarinen@student.oulu.fi>
2023-05-17 14:58:58 +0300

    Tehtävät 1.1 - 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -5,19 +5,24 @@ const Header = (props) => {
 	)
 }
 
-const Content = (props) => {
+const Part = (props) => {
 	console.log(props)
 	return (
 		<div>
 			<p>
-				{props.part1} {props.exercises1}
+				{props.part} {props.exercises}
 			</p>
-			<p>
-				{props.part2} {props.exercises2}
-			</p>
-			<p>
-				{props.part3} {props.exercises3}
-			</p>
+		</div>
+	)
+}
+
+const Content = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<Part part={props.part1} exercises={props.exercises1} />
+			<Part part={props.part2} exercises={props.exercises2} />
+			<Part part={props.part3} exercises={props.exercises3} />
 		</div>
 	)
 }
@@ -45,8 +50,10 @@ const App = () => {
 	return (
 		<div>
 			<Header course={course} />
-			<Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-			<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+			<Content part1={part1} exercises1={exercises1}
+			part2={part2} exercises2={exercises2}
+			part3={part3} exercises3={exercises3} />
+			<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
 		</div>
 	)
 }
